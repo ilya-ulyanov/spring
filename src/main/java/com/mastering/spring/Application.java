1809735b1793d22cb33f4b53f5f6e92dd9333066
Illia Ulianov <ulyanov.iv@gmail.com>
2019-11-18 00:46:49 +0100

    [DI] using XML based context configuration

--- src/main/java/com/mastering/spring/Application.java
+++ src/main/java/com/mastering/spring/Application.java
@@ -5,6 +5,7 @@ import com.mastering.spring.service.BusinessService;
 import com.mastering.spring.service.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,7 +13,9 @@ public class Application {
 //        BusinessService service = new BusinessServiceImpl(dataService);
 
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(SpringContextConfiguration.class);
+//                new AnnotationConfigApplicationContext(SpringContextConfiguration.class);
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
         BusinessService service = context.getBean(BusinessService.class);
         System.out.println(service.calculateSum(new User()));
     }
