0b984c56e1f6af1705030ca5afec0b9ff559b14b
Illia Ulianov <ulyanov.iv@gmail.com>
2019-11-18 00:57:21 +0100

    [DI] resolving ambiguity during resolution process with @Primary annotation when two or more matching beans are found

--- src/main/java/com/mastering/spring/data/DataServiceImpl2.java
+++ src/main/java/com/mastering/spring/data/DataServiceImpl2.java
@@ -1,17 +1,14 @@
 package com.mastering.spring.data;
 
 import com.mastering.spring.service.User;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Primary
 @Repository
-public class DataServiceImpl implements DataService {
+public class DataServiceImpl2 implements DataService {
     @Override
     public List<Data> retrieveData(User user) {
-        return Arrays.asList(new Data(10));
+        return null;
     }
 }
