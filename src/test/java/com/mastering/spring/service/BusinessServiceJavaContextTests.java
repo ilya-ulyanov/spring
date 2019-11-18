c5ad7d2fd7a3c9fdad8fa404920cccce0cc70721
Illia Ulianov <ulyanov.iv@gmail.com>
2019-11-18 01:20:31 +0100

    [DI] writing unit tests based on Spring context

---
+++ src/test/java/com/mastering/spring/service/BusinessServiceJavaContextTests.java
@@ -0,0 +1,24 @@
+package com.mastering.spring.service;
+
+import com.mastering.spring.config.SpringContextConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { SpringContextConfiguration.class })
+public class BusinessServiceJavaContextTests {
+    @Autowired
+    private BusinessService businessService;
+
+    @Test
+    public void testCalculateSum() {
+        long sum = this.businessService.calculateSum(new User());
+        assertThat(sum, is(20L));
+    }
+}
---
+++ src/test/java/com/mastering/spring/service/BusinessServiceXmlContextTests.java
@@ -0,0 +1,23 @@
+package com.mastering.spring.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
+public class BusinessServiceXmlContextTests {
+    @Autowired
+    private BusinessService businessService;
+
+    @Test
+    public void testCalculateSum() {
+        long sum = this.businessService.calculateSum(new User());
+        assertThat(sum, is(20L));
+    }
+}
